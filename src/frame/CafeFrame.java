901aab83df0cff7b8ef5b40c09a7d5b1416ff45b
Better Jeong <eunjeong42na@gmail.com>
2022-12-16 11:49:19 +0900

    Update CafeFrame.java

--- src/frame/CafeFrame.java
+++ src/frame/CafeFrame.java
@@ -7,7 +7,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.util.ArrayList;
 
 public class CafeFrame extends JFrame implements ActionListener {
 
